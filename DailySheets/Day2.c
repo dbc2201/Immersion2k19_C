93dfb04cca4d331576d7712b371ca5c7d4cd8eb2
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-03 23:16:54 +0530

    D2 Q2

--- DailySheets/Day2.c
+++ DailySheets/Day2.c
@@ -17,10 +17,20 @@
 	return 0;
 }*/
 
-/*	Question
- *
- * 	Answer:
+/*	Question 2
+ *	Which of the following is not a valid variable name in C programming?
+ * 	Answer: 4
  * */
+
+/*int main() {
+	int number;
+	float rate;
+	int variable_count;
+	int $main;
+	printf("%d %f %d %d", number, rate, variable_count, $main);
+	return 0;
+}*/
+
 /*	Question
  *
  * 	Answer:
