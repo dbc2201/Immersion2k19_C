f030eb579fb15f653ec2a87ff6354b1267019073
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-03 15:59:00 +0530

    day 5, question 1

---
+++ DailySheets/Day5.c
@@ -0,0 +1,118 @@
+//
+// Created by divyanshbhardwaj.
+//
+
+#include <stdio.h>
+
+/*	Question 1
+ *	What would be the output of the following C code?
+ * 	Answer: 1
+ * */
+
+/*int main() {
+	int x = 5;
+	if (x < 1) {
+		printf("Hello");
+	}
+	if (x == 5) {
+		printf("hi");
+	} else {
+		printf("No");
+	}
+	return 0;
+}*/
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
