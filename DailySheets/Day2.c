4b18c600fdf93c2da33697058b40eb2258a8f519
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-03 23:15:02 +0530

    D2 Q1

--- DailySheets/Day2.c
+++ DailySheets/Day2.c
@@ -4,10 +4,19 @@
 
 #include <stdio.h>
 
-/*	Question
- *
- * 	Answer:
+/*	Question 1
+ *	Which of the following is not a valid variable name declaration?
+ * 	Answer: 4
  * */
+
+/*int main() {
+	int _a3 = 5;
+	int _3a = 10;
+	int _A3 = 15;
+	printf("%d %d %d", _a3, _3a, _A3);
+	return 0;
+}*/
+
 /*	Question
  *
  * 	Answer:
