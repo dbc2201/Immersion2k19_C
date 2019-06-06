73dd3c91b8d51a6543ab446eece5ee01a3973eef
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-06 11:02:20 +0530

    day3 q5

--- DailySheets/Day3.c
+++ DailySheets/Day3.c
@@ -61,10 +61,20 @@ int main() {
 	return 0;
 }*/
 
-/*	Question
- *
- * 	Answer:
+/*	Question 5
+ *	What would be the output of the following C code?
+ * 	Answer: D
  * */
+
+/*#include <stdio.h>
+int main() {
+	int max-val = 100;
+	int min-val = 10;
+	int avg-val;
+	avg-val = max-val + min-val / 2;
+	printf(“%d”, avg-val);
+	return 0;
+}*/
 
 /*	Question
  *
