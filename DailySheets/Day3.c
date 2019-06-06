fe0631ea28df7838022467990480107388d35169
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-06 11:00:40 +0530

    day3 q3

---
+++ DailySheets/Day3.c
@@ -0,0 +1,134 @@
+//
+// Created by divyanshbhardwaj
+//
+
+#include <stdio.h>
+
+/*	Question 1
+ * 	What would be the output of the following code statement?
+	printf(“%d”, printf(“Jon Snow”));
+ * 	Answer: A
+ * */
+
+/*int main() {
+	printf("%d", printf("Jon Snow"));
+	return 0;
+}*/
+
+/*	Question 2
+ *	What would be the output of the following C code?
+ * 	Answer: B
+ * */
+
+/*#include <stdio.h>
+int main() {
+	float a = 0.5f, b = 0.7f;
+	if ( b < 0.8 ) {
+		if ( a < 0.5 ) {
+			printf("ABCD");
+		} else {
+			printf("PQR");
+		}
+	} else {
+		printf("JKLF");
+	}
+	return 0;
+}*/
+
+
+/*	Question 3
+ *	What would be the output of the following C code?
+ * 	Answer: B
+ * */
+
+/*#include <stdio.h>
+int main() {
+	int var = 010;
+	printf("%d", var);
+	return 0;
+}*/
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
+
+/*	Question
+ *
+ * 	Answer:
+ * */
