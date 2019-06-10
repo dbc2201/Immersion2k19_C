7ea3d4c594485c25830e75bf5f0fd754a50ec0b1
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-10 10:19:01 +0530

    day5.c: add question 1

--- DailySheets/Day5.c
+++ DailySheets/Day5.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 /*	Question 1
- *
+ *	What would be the output of the following C code?
  * */
+
+/*#include <stdio.h>
+int main() {
+	int x = 2, y = 0;
+	int z = (y++) ? y == 1 && x : 0;
+	printf("%d\n", z);
+	return 0;
+}*/
 
 /*	Answer 1: */
 
