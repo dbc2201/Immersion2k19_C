6a6991df44589c98a1bf38fa00a8a8264cf501bc
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-10 10:24:58 +0530

    day5.c: add question 2, 3

--- DailySheets/Day5.c
+++ DailySheets/Day5.c
@@ -18,11 +18,27 @@ int main() {
  *
  * */
 
+/*#include <stdio.h>
+int main() {
+	int a = 2;
+	int b = 0;
+	int y = (b == 0) ? a :(a > b) ? (b = 1): a;
+	printf("%d\n", y);
+}*/
+
 /*	Answer 1: */
 
 /*	Question 1
  *
  * */
+
+/*#include <stdio.h>
+void main() {
+	int k = 8;
+	int m = 7;
+	int z = k < m ? k++ : m++;
+	printf("%d", z);
+}*/
 
 /*	Answer 1: */
 
