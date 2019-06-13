0b6908a0a999ed6cc3855f05d7165da1954a3f58
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-13 10:25:02 +0530

    Day2.c: add Q1, 2

---
+++ DailySheets/Day7.c
@@ -0,0 +1,32 @@
+//
+// Created by Divyansh Bhardwaj, CEA, IET, GLAU
+//
+#include <stdio.h>
+
+/*	Question 1
+ * 	What is the output of this C code?
+ * */
+
+/*
+int main() {
+	while ()
+		printf("In while loop");
+	printf("After while loop");
+}*/
+
+//	Answer : C (ERROR)
+
+/*	Question 2
+ * 	What is the output of this C code?
+ * */
+
+/*
+int main() {
+	do
+		printf("In do-while loop ");
+	while (0);
+	printf("After loop\n");
+}*/
+
+//	Answer : B (In do-while loop After loop)
+
