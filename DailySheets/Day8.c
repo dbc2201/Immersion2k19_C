04a1e569416ee117c46767d467ec3f46f3ee3123
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-13 11:42:27 +0530

    Day8.c: add Q1

---
+++ DailySheets/Day8.c
@@ -0,0 +1,24 @@
+//
+// Created by Divyansh Bhardwaj
+//
+
+#include <stdio.h>
+
+/*	Question 1
+ * 	How many times is "*" printed?
+ * */
+
+/*
+int main() {
+	int x;
+	for (x = -1; x <= 10; x++) {
+		if (x < 5)
+			continue;
+		else
+			break;
+		printf("*");
+	}
+	return 0;
+}*/
+
+//	Answer 1 : B (0 times)
