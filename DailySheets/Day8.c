6d22c56a95e511ae19afea57d91341f48b42c7cf
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-06-13 12:16:38 +0530

    Day8.c: add Q10

--- DailySheets/Day8.c
+++ DailySheets/Day8.c
@@ -142,3 +142,21 @@ void main() {
 }*/
 
 //	Answer 9 : B (Hi is printed 8 times)
+
+/*	Question 10
+ * 	What is the output of this C code?
+ * */
+
+/*
+int main() {
+	int i = 0;
+	do {
+		i++;
+		if (i == 2)
+			continue;
+		printf("In while loop ");
+	} while (i < 2);
+	printf("%d\n", i);
+}*/
+
+//	Answer 10 : A (In while loop 2)
